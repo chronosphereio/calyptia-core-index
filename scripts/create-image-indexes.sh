953f1e98b848e1b492f5398fb91e391ce2b50c25
Jorge Niedbalski <j@calyptia.com>
2022-07-13 17:49:42 +0200

    initial pr
    Signed-off-by: Jorge Niedbalski <j@calyptia.com>

--- scripts/create-image-indexes.sh
+++ scripts/create-image-indexes.sh
@@ -5,6 +5,6 @@ GITHUB_TOKEN=${GITHUB_TOKEN:?}
 CONTAINER_PACKAGE=${CONTAINER_PACKAGE:-calyptia/core}
 CONTAINER_INDEX_FILE=${CONTAINER_INDEX_FILE:-container.index.json}
 
-export GHCR_TOKEN=$(echo "$GITHUB_TOKEN" | base64)
+GHCR_TOKEN=$(echo "$GITHUB_TOKEN" | base64)
 
 curl --silent -H "Authorization: Bearer $GHCR_TOKEN" https://ghcr.io/v2/"${CONTAINER_PACKAGE}"/tags/list | jq -r '.tags' > "$CONTAINER_INDEX_FILE" && cat "$CONTAINER_INDEX_FILE"
