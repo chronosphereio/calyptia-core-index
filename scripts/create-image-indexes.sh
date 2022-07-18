2b50b902e33d97028b7ffae72afa06a363120ad2
Patrick Stephens <pat@calyptia.com>
2022-07-18 12:48:26 +0100

    scripts: add image creation for GCP
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- scripts/create-image-indexes.sh
+++ scripts/create-image-indexes.sh
@@ -5,6 +5,18 @@ GITHUB_TOKEN=${GITHUB_TOKEN:?}
 CONTAINER_PACKAGE=${CONTAINER_PACKAGE:-calyptia/core}
 CONTAINER_INDEX_FILE=${CONTAINER_INDEX_FILE:-container.index.json}
 
+GCP_IMAGE_LABEL=${GCP_IMAGE_LABEL:-calyptia-core-release}
+GCP_INDEX_FILE=${GCP_INDEX_FILE:-gcp.index.json}
+AWS_INDEX_FILE=${AWS_INDEX_FILE:-aws.index.json}
+
+# Assumption for AWS and GCP is authentication is complete prior to this script
+
 GHCR_TOKEN=$(echo "$GITHUB_TOKEN" | base64)
 
-curl --silent -H "Authorization: Bearer $GHCR_TOKEN" https://ghcr.io/v2/"${CONTAINER_PACKAGE}"/tags/list | jq -r '.tags | map(select(.| test("^v(.*)")))' > "$CONTAINER_INDEX_FILE" && cat "$CONTAINER_INDEX_FILE"
+curl --silent -H "Authorization: Bearer $GHCR_TOKEN" https://ghcr.io/v2/"${CONTAINER_PACKAGE}"/tags/list | \
+    jq -r '.tags | map(select(.| test("^v(.*)")))' | tee "$CONTAINER_INDEX_FILE"
+
+# Sort by most recent any images with an appropriate label specified
+gcloud compute images list --no-standard-images --sort-by='~creationTimestamp' \
+--filter="labels.$GCP_IMAGE_LABEL ~ .+" --format='json(name,labels)' | tee "$GCP_INDEX_FILE"
+
