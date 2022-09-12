a138598e69a8bd0a243ab1a11be0f90cc0592e06
Patrick Stephens <pat@calyptia.com>
2022-09-12 10:30:37 +0100

    container: switch to Github API calls
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- scripts/create-container-index.sh
+++ scripts/create-container-index.sh
@@ -5,7 +5,5 @@ GITHUB_TOKEN=${GITHUB_TOKEN:?}
 CONTAINER_PACKAGE=${CONTAINER_PACKAGE:-calyptia/core}
 CONTAINER_INDEX_FILE=${CONTAINER_INDEX_FILE:-container.index.json}
 
-GHCR_TOKEN=$(echo "$GITHUB_TOKEN" | base64)
-
-curl --silent -H "Authorization: Bearer $GHCR_TOKEN" https://ghcr.io/v2/"${CONTAINER_PACKAGE}"/tags/list | \
-    jq -r '.tags | map(select(.| test("^v(.*)")))' | tee "$CONTAINER_INDEX_FILE"
+curl -sSfL -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/"${CONTAINER_PACKAGE}"/tags | \
+    jq '[.[].name|select(.|test("^v(.*)"))]' | tee "$CONTAINER_INDEX_FILE"
