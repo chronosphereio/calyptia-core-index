77c00c683ccf344cf2a14b0b22d1461ae969f82e
Pat <pat@calyptia.com>
2022-09-12 13:44:52 +0100

    container: fix missing tags (#49)
    * container: switch to Github API calls
    
    Signed-off-by: Patrick Stephens <pat@calyptia.com>
    
    * container: update pagination call
    
    Signed-off-by: Patrick Stephens <pat@calyptia.com>
    
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- scripts/create-container-index.sh
+++ scripts/create-container-index.sh
@@ -7,5 +7,5 @@ CONTAINER_INDEX_FILE=${CONTAINER_INDEX_FILE:-container.index.json}
 
 GHCR_TOKEN=$(echo "$GITHUB_TOKEN" | base64)
 
-curl --silent -H "Authorization: Bearer $GHCR_TOKEN" https://ghcr.io/v2/"${CONTAINER_PACKAGE}"/tags/list | \
+curl --silent -H "Authorization: Bearer $GHCR_TOKEN" https://ghcr.io/v2/"${CONTAINER_PACKAGE}"/tags/list?n=100000 | \
     jq -r '.tags | map(select(.| test("^v(.*)")))' | tee "$CONTAINER_INDEX_FILE"
