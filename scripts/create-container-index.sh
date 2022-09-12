2775020b1d2b72fc5788a43a24640bf19db15bbf
Patrick Stephens <pat@calyptia.com>
2022-09-12 10:48:53 +0100

    container: update pagination call
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- scripts/create-container-index.sh
+++ scripts/create-container-index.sh
@@ -5,5 +5,7 @@ GITHUB_TOKEN=${GITHUB_TOKEN:?}
 CONTAINER_PACKAGE=${CONTAINER_PACKAGE:-calyptia/core}
 CONTAINER_INDEX_FILE=${CONTAINER_INDEX_FILE:-container.index.json}
 
-curl -sSfL -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/"${CONTAINER_PACKAGE}"/tags | \
-    jq '[.[].name|select(.|test("^v(.*)"))]' | tee "$CONTAINER_INDEX_FILE"
+GHCR_TOKEN=$(echo "$GITHUB_TOKEN" | base64)
+
+curl --silent -H "Authorization: Bearer $GHCR_TOKEN" https://ghcr.io/v2/"${CONTAINER_PACKAGE}"/tags/list?n=100000 | \
+    jq -r '.tags | map(select(.| test("^v(.*)")))' | tee "$CONTAINER_INDEX_FILE"
