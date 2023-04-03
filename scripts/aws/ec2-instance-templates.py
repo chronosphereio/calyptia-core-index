2964c48506a9b108db99416cce9f8d7d6bad4059
Patrick Stephens <pat@calyptia.com>
2023-04-03 13:56:53 +0100

    aws: update for SSH and SSM access changes
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- scripts/aws/ec2-instance-templates.py
+++ scripts/aws/ec2-instance-templates.py
@@ -26,7 +26,7 @@ from troposphere import (
     Join,
     If
 )
-from troposphere.ec2 import Instance, SecurityGroup
+from troposphere.ec2 import Instance, SecurityGroup, SecurityGroupRule
 from troposphere.iam import Role, InstanceProfile, PolicyType
 
 
@@ -61,13 +61,14 @@ def create_mapping(calyptia_images_index: list[dict]) -> dict:
             _region_dict = regions_mappings[_region]
 
         _release_version = image_def["release"].replace(".", "")
-        if _release_version not in _region_dict:
-            _region_dict[_release_version] = image_def["ImageId"]
+        if not _release_version.startswith("0"):
+            if _release_version not in _region_dict:
+                _region_dict[_release_version] = image_def["ImageId"]
 
     return regions_mappings
 
 
-def create_template(amis_mapping: dict) -> Template:
+def create_ssm_template(amis_mapping: dict) -> Template:
     """Creates the CFN Template"""
     allowed_versions: list[str] = []
     for _region, _versions in amis_mapping.items():
@@ -75,10 +76,16 @@ def create_template(amis_mapping: dict) -> Template:
             if _version not in allowed_versions:
                 allowed_versions.append(_version)
 
-    template = Template("EC2 Instance with Fluent-Bit by Calyptia")
+    template = Template("Calyptia Core EC2 Instance")
     template.add_mapping("RegionsAmis", amis_mapping)
     version_id = template.add_parameter(
         Parameter("Version", Type="String", AllowedValues=allowed_versions)
+    )
+    project_token = template.add_parameter(
+        Parameter("CalyptiaCoreToken", Type="String")
+    )
+    instance_type = template.add_parameter(
+        Parameter("InstanceType", Type="String")
     )
     permissions_boundary = template.add_parameter(
         Parameter(
@@ -89,10 +96,13 @@ def create_template(amis_mapping: dict) -> Template:
         )
     )
     template.add_condition(
-        "UsePermissionsBoundary", Not(Equals(Ref(permissions_boundary), "none"))
+        "UsePermissionsBoundary", Not(
+            Equals(Ref(permissions_boundary), "none"))
     )
-    vpc_id = template.add_parameter(Parameter("VpcId", Type="AWS::EC2::VPC::Id"))
-    subnet = template.add_parameter(Parameter("SubnetId", Type="AWS::EC2::Subnet::Id"))
+    vpc_id = template.add_parameter(
+        Parameter("VpcId", Type="AWS::EC2::VPC::Id"))
+    subnet = template.add_parameter(
+        Parameter("SubnetId", Type="AWS::EC2::Subnet::Id"))
     instance_sg = template.add_resource(
         SecurityGroup(
             "InstanceSecurityGroup",
@@ -103,11 +113,12 @@ def create_template(amis_mapping: dict) -> Template:
     iam_role = template.add_resource(
         Role(
             "InstanceRole",
-            PermissionsBoundary=If("UsePermissionsBoundary", Ref(permissions_boundary), Ref("AWS::NoValue")),
+            PermissionsBoundary=If("UsePermissionsBoundary", Ref(
+                permissions_boundary), Ref("AWS::NoValue")),
             ManagedPolicyArns=[
                 "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
             ],
-            Description=Sub("${AWS::StackName} - FluentBit getting-started"),
+            Description=Sub("${AWS::StackName} - Calyptia Core"),
             AssumeRolePolicyDocument={
                 "Version": "2012-10-17",
                 "Statement": [
@@ -134,26 +145,91 @@ def create_template(amis_mapping: dict) -> Template:
                 instance_profile.title,
             ],
             ImageId=FindInMap("RegionsAmis", Region, Ref(version_id)),
+            InstanceType=Ref(instance_type),
             SubnetId=Ref(subnet),
-            SecurityGroups=[GetAtt(instance_sg, "GroupId")],
+            SecurityGroupIds=[GetAtt(instance_sg, "GroupId")],
             IamInstanceProfile=Ref(instance_profile),
             UserData=Base64(
-                """#!/bin/bash
-                mkdir /tmp/ssm
-                cd /tmp/ssm
-                wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
-                sudo dpkg -i amazon-ssm-agent.deb
-                sudo systemctl enable amazon-ssm-agent
-                """
-            ),
+                Join("=", ["CALYPTIA_CLOUD_PROJECT_TOKEN", Ref(project_token)])),
         )
     )
     return template
 
 
-def render_template(mappings: dict, output_path: str) -> None:
+def create_ssh_template(amis_mapping: dict) -> Template:
+    """Creates the CFN Template"""
+    allowed_versions: list[str] = []
+    for _region, _versions in amis_mapping.items():
+        for _version in _versions:
+            if _version not in allowed_versions:
+                allowed_versions.append(_version)
+
+    template = Template("Calyptia Core EC2 Instance with SSH access")
+    template.add_mapping("RegionsAmis", amis_mapping)
+    version_id = template.add_parameter(
+        Parameter("Version", Type="String", AllowedValues=allowed_versions)
+    )
+    project_token = template.add_parameter(
+        Parameter("CalyptiaCoreToken", Type="String")
+    )
+    instance_type = template.add_parameter(
+        Parameter("InstanceType", Type="String")
+    )
+    ssh_location = template.add_parameter(
+        Parameter("SSHLocation", Type="String", MinLength=9,
+                  MaxLength=18, Default="0.0.0.0/0")
+    )
+    key_name = template.add_parameter(
+        Parameter("KeyName", Type="AWS::EC2::KeyPair::KeyName",
+                  ConstraintDescription="Must be the name of an existing EC2 KeyPair.")
+    )
+
+    instance_sg = template.add_resource(
+        SecurityGroup(
+            "InstanceSecurityGroup",
+            GroupDescription=Sub("${AWS::StackName} - CalyptiaCore"),
+            SecurityGroupIngress=[
+                SecurityGroupRule(
+                    IpProtocol="tcp",
+                    FromPort=22,
+                    ToPort=22,
+                    CidrIp=Ref(ssh_location),
+                ),
+            ]
+        )
+    )
+
+    instance = template.add_resource(
+        Instance(
+            "CalyptiaCore",
+            ImageId=FindInMap("RegionsAmis", Region, Ref(version_id)),
+            InstanceType=Ref(instance_type),
+            SecurityGroupIds=[GetAtt(instance_sg, "GroupId")],
+            KeyName=Ref(key_name),
+            UserData=Base64(
+                Join("=", ["CALYPTIA_CLOUD_PROJECT_TOKEN", Ref(project_token)])),
+        )
+    )
+    return template
+
+
+def render_ssm_template(mappings: dict, output_path: str) -> None:
     """Generates template, validates if possible, writes to file"""
-    template = create_template(mappings)
+    template = create_ssm_template(mappings)
+    try:
+        import boto3
+
+        cfn = boto3.client("cloudformation")
+        cfn.validate_template(TemplateBody=template.to_json())
+    except Exception as error:
+        print(error)
+    with open(path.abspath(output_path), "w") as template_fd:
+        template_fd.write(template.to_yaml())
+
+
+def render_ssh_template(mappings: dict, output_path: str) -> None:
+    """Generates template, validates if possible, writes to file"""
+    template = create_ssh_template(mappings)
     try:
         import boto3
 
@@ -168,15 +244,19 @@ def render_template(mappings: dict, output_path: str) -> None:
 def main():
     from argparse import ArgumentParser
 
-    parser = ArgumentParser("Generate demo EC2 template for Core")
-    parser.add_argument("-f", "--file", help="Path to the index file", type=str)
+    parser = ArgumentParser("Generate EC2 template for Calyptia Core")
+    parser.add_argument(
+        "-f", "--file", help="Path to the index file", type=str)
     parser.add_argument(
         "--url",
         help="URL to the images index",
         default="https://raw.githubusercontent.com/calyptia/core-images-index/main/aws.index.json",
     )
     parser.add_argument(
-        "-o", "--output", help="Output template path", type=str, default="calyptia.yaml"
+        "--outputssm", help="Output SSM template path", type=str, default="calyptia-ssm.yaml"
+    )
+    parser.add_argument(
+        "--outputssh", help="Output SSH template path", type=str, default="calyptia-ssh.yaml"
     )
 
     args = parser.parse_args()
@@ -184,7 +264,8 @@ def main():
         mappings = create_mapping(get_definition_from_file(args.file))
     else:
         mappings = create_mapping(get_latest_definition_from_url(args.url))
-    render_template(mappings, args.output)
+    render_ssm_template(mappings, args.outputssm)
+    render_ssh_template(mappings, args.outputssh)
 
 
 if __name__ == "__main__":
