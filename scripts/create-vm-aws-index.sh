9ac74fa4421c10bb50c221080a8911618b5a9add
Patrick Stephens <pat@calyptia.com>
2023-11-20 15:14:02 +0000

    scripts: fix image key for AWS VMs
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- scripts/create-vm-aws-index.sh
+++ scripts/create-vm-aws-index.sh
@@ -16,7 +16,7 @@ for aws_region in $(aws ec2 describe-regions --output text | cut -f4)
 do
     # As we only want a single image per tag value we need to first get our tag values then query for each one and take the latest
     aws ec2 describe-tags --no-paginate --region "$aws_region" \
-        --filter "Name=key,Values=calyptia-core-release" --query 'Tags[*]' --output json | jq -cr 'unique_by(.Key,.Value)|.[].Value' > "$aws_region".tags
+        --filter "Name=key,Values=$IMAGE_KEY" --query 'Tags[*]' --output json | jq -cr 'unique_by(.Key,.Value)|.[].Value' > "$aws_region".tags
 
     # Get our images with this tag value and region, sort by creation date and select last for most recent
     # Add region + release info unless we have no release in that region (null response)
